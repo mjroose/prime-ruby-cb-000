4918097b7cba275eae792facc79bb524a6030d67
Michael Roose <mjroose@gmail.com>
2018-08-05 20:43:46 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  elsif number <= 3
+    return true
+  elsif n.even? || n % 3 == 0
+    return false
+  i = 5
+  while i * i <= number
+    if number % i == 0 || number % (i + 2) = 0
+      return false
+    i += 6
+  end
+  return true
+end
