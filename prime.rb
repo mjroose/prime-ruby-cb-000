5c504ab2c22637945fda7a95895dd17697d4b7b5
Michael Roose <mjroose@gmail.com>
2018-08-05 20:46:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  elsif number <= 3
+    return true
+  elsif number.even? || number % 3 == 0
+    return false
+  end
+
+  i = 5
+  while i * i <= number
+    if number % i == 0 || number % (i + 2) == 0
+      return false
+    end
+    i += 6
+  end
+  return true
+end
+
+puts prime?(97)
